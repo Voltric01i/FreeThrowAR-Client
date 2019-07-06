fb85b9438b91734a3d4f73d91000dfc0b7235848
Yuki <yupam0922@gmail.com>
2019-07-06 11:24:45 +0900

    現状動くやつ

---
+++ Assets/Script/GoalAnimater.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalAnimater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject GoalRing;
+    Animator gr;
+
+    void Start()
+    {
+        gr = GoalRing.GetComponent<Animator>();
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void bigger(){
+        Debug.Log("60 ");
+        gr.SetTrigger("100to200");
+    }
+
+    public void smaller(){
+        Debug.Log("120 ");
+        gr.SetTrigger("70to100");
+    }
+}
